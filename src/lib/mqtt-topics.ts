eaf350f4932a663ac8c4a2641fc866fc941f5078
102-301 <clickjeho1@gmail.com>
2026-04-22 10:58:50 +0900

    Update mqtt-topics.ts

--- src/lib/mqtt-topics.ts
+++ src/lib/mqtt-topics.ts
@@ -1,4 +1,3 @@
-/** 아두이노 스케치와 동일한 MQTT 토픽 (한 곳에서만 수정) */
 export const MQTT_TOPIC_TEMP = "smartfarm/sensor/temp";
 export const MQTT_TOPIC_HUMI = "smartfarm/sensor/humi";
 export const MQTT_TOPIC_STATUS = "smartfarm/status";
