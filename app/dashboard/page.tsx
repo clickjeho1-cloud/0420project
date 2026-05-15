8a2a90abd8551c01c654f1568c2584ea829b91a4
0418project <clickjeho1@gmail.com>
2026-05-15 13:55:51 +0900

    update

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -239,7 +239,7 @@ export default function Page() {
             <div className="video-container">
               {ytUrl ? (
                 getYoutubeId(ytUrl) ? (
-                  <iframe width="100%" height="100%" src={`https://www.youtube.com/embed/${getYoutubeId(ytUrl)}?autoplay=1&mute=1`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+                  <iframe width="100%" height="100%" src={`https://www.youtube.com/embed/${getYoutubeId(ytUrl)}?autoplay=1`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
                 ) : (
                   <img src={ytUrl} alt="Stream" crossOrigin="anonymous" />
                 )
