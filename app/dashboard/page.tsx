d412c5278aa3a92adc767a32c2e3a399481d1d0e
0418project <clickjeho1@gmail.com>
2026-05-22 12:54:21 +0900

    update

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -182,7 +182,7 @@ export default function Page() {
 
   // 유튜브 링크에서 ID를 추출하는 함수
   const getYoutubeId = (url: string) => {
-    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=|live\/)([^#\&\?]*).*/;
+    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=|live\/|shorts\/)([^#\&\?]*).*/;
     const match = url.match(regExp);
     return (match && match[2].length === 11) ? match[2] : null;
   };
